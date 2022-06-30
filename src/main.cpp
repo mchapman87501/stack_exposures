17fd12b2f58ed1591743b3bedb942a016b7c685c
Mitchell Chapman <mchapman87501@gmail.com>
2022-06-30 14:40:46 -0600

    Load all images simultaneously -- a bad idea given memory consumption.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <sstream>
 
+#include <deque>
+#include <future>
+#include <thread>
+
 #include <opencv2/imgcodecs.hpp>
 
 #include "image_aligner.hpp"
@@ -9,83 +13,90 @@
 #include "image_stacker.hpp"
 
 namespace {
-    using namespace std;
+using namespace std;
 
-    filesystem::path default_outpath("stacked.tiff");
+filesystem::path default_outpath("stacked.tiff");
 
-    struct Options {
-        string m_invoked_as;
-        bool m_align;
-        filesystem::path m_outpath;
-        vector<filesystem::path> m_input_images;
+struct Options {
+  string m_invoked_as;
+  bool m_align;
+  filesystem::path m_outpath;
+  vector<filesystem::path> m_input_images;
 
-        Options(int argc, char *argv[]):
-            m_align(true), m_outpath(default_outpath) {
+  Options(int argc, char *argv[]) : m_align(true), m_outpath(default_outpath) {
 
-            m_invoked_as = string(argv[0]);
-            int i = 1;
-            while (i < argc) {
-                string curr(argv[i]);
-                if (curr == "-o") {
-                    i++;
-                    m_outpath = filesystem::path(argv[i]);
-                // TODO handle "--output", "--output="
-                } else if (curr == "--no-align") {
-                    m_align = false;
-                } else if ((curr == "-h") || (curr == "--help")) {
-                    show_help();
-                } else if (curr.front() == '-') {
-                    ostringstream outs;
-                    outs << "Unknown option '" << curr << "'.";
-                    show_help(outs.str());
-                } else {
-                    m_input_images.push_back(filesystem::path(curr));
-                }
-                i++;
-            }
+    m_invoked_as = string(argv[0]);
+    int i = 1;
+    while (i < argc) {
+      string curr(argv[i]);
+      if (curr == "-o") {
+        i++;
+        m_outpath = filesystem::path(argv[i]);
+        // TODO handle "--output", "--output="
+      } else if (curr == "--no-align") {
+        m_align = false;
+      } else if ((curr == "-h") || (curr == "--help")) {
+        show_help();
+      } else if (curr.front() == '-') {
+        ostringstream outs;
+        outs << "Unknown option '" << curr << "'.";
+        show_help(outs.str());
+      } else {
+        m_input_images.push_back(filesystem::path(curr));
+      }
+      i++;
+    }
 
-            if (m_input_images.size() < 1) {
-                show_help(cerr, 1);
-            }
-        }
+    if (m_input_images.size() < 1) {
+      show_help(cerr, 1);
+    }
+  }
 
 private:
-        string padded_right(const string& s, size_t width) {
-            if (width <= s.size()) {
-                return s;
-            }
-            size_t pad_count(width - s.size());
-            string padding(pad_count, ' ');
-            return s + padding;
-        }
+  string padded_right(const string &s, size_t width) {
+    if (width <= s.size()) {
+      return s;
+    }
+    size_t pad_count(width - s.size());
+    string padding(pad_count, ' ');
+    return s + padding;
+  }
 
-        string padded_arg_help(const string arg, const string& help_msg, size_t pad_width = 20) {
-            return padded_right(arg, pad_width) + help_msg;
-        }
+  string padded_arg_help(const string arg, const string &help_msg,
+                         size_t pad_width = 20) {
+    return padded_right(arg, pad_width) + help_msg;
+  }
 
-        void show_help(ostream& outs = cout, int exit_code = 0) {
-            // O for std::format
-            ostringstream op_outs;
-            op_outs << "Save result to <path>; default " << default_outpath << ".";
-            const string output_path_help(op_outs.str());
+  void show_help(ostream &outs = cout, int exit_code = 0) {
+    // O for std::format
+    ostringstream op_outs;
+    op_outs << "Save result to <path>; default " << default_outpath << ".";
+    const string output_path_help(op_outs.str());
 
-            outs << "Usage: " << m_invoked_as << " [-o path] [--no-align] [-h|--help] image_filaname [image_filename...]" << endl
-            << "Options:" << endl
-            << padded_arg_help("  -o <path>", output_path_help) << endl
-            << padded_arg_help("  --no-align", "Do not align images before stacking.") << endl
-            << padded_arg_help("  -h, --help", "Show this help message and exit.") << endl
-            << "Arguments:" << endl
-            << padded_arg_help("  <image_filename>", "path of an image to stack") << endl;
-            std::exit(exit_code);
-        }
+    outs << "Usage: " << m_invoked_as
+         << " [-o path] [--no-align] [-h|--help] image_filaname "
+            "[image_filename...]"
+         << endl
+         << "Options:" << endl
+         << padded_arg_help("  -o <path>", output_path_help) << endl
+         << padded_arg_help("  --no-align",
+                            "Do not align images before stacking.")
+         << endl
+         << padded_arg_help("  -h, --help", "Show this help message and exit.")
+         << endl
+         << "Arguments:" << endl
+         << padded_arg_help("  <image_filename>", "path of an image to stack")
+         << endl;
+    std::exit(exit_code);
+  }
 
-        void show_help(const string& error_msg) {
-            cerr << "Error: " << error_msg << endl;
-            show_help(cerr, 1);
-        }
-    };
+  void show_help(const string &error_msg) {
+    cerr << "Error: " << error_msg << endl;
+    show_help(cerr, 1);
+  }
+};
 
-}
+} // namespace
 
 int main(int argc, char *argv[]) {
   using namespace std;
@@ -93,20 +104,35 @@ int main(int argc, char *argv[]) {
 
   Options opts(argc, argv);
 
-  ImageLoader loader;
   ImageAligner aligner;
   ImageStacker stacker;
 
+  using ImageInfoFuture = shared_future<ImageInfo::Ptr>;
+
+  deque<ImageInfoFuture> load_tasks;
+  // TODO use a thread pool instead of launching all background loads at once.
+  for (const auto image_path : opts.m_input_images) {
+    ImageLoader loader;
+    // Support concurrent loading/alignment/stacking, while
+    // respecting stacking order.
+    auto load_async = [image_path]() {
+      ImageLoader loader;
+      auto result = loader.load_image(image_path);
+      cout << image_path << endl << flush;
+      return result;
+    };
+    load_tasks.push_back(async(launch::async, load_async));
+  }
+
   bool is_first = true;
-  for (const auto image_path: opts.m_input_images) {
-    cout << image_path << endl;
+  while (!load_tasks.empty()) {
+    auto fut = load_tasks.front();
+    load_tasks.pop_front();
+    auto img_info = fut.get();
 
     // TODO get the icc profile from the first image.
     if (is_first) {
     }
-
-    // TODO support concurrent loading/alignment/stacking.
-    auto img_info = loader.load_image(image_path);
 
     auto aligned = opts.m_align ? aligner.align(img_info) : img_info;
     stacker.push(aligned->image());
