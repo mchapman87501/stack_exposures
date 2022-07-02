252d3af8e52d5168a6be4b7910efc958c7a6f53a
Mitchell Chapman <mchapman87501@gmail.com>
2022-07-02 09:44:58 -0600

    Test unalignable images.

--- tests/src/test_image_aligner.cpp
+++ tests/src/test_image_aligner.cpp
@@ -1,18 +1,18 @@
 #include "image_aligner.hpp"
+#include "image_loader.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <string>
 
-
 namespace {
-  auto dummy_image(size_t width, size_t height) {
+auto dummy_image(size_t width, size_t height) {
   using namespace StackExposures;
 
   std::filesystem::path path("/no/such/image.jpg");
   cv::Mat img(height, width, CV_8UC3);
   return std::make_shared<ImageInfo>(path, img);
 }
-}
+} // namespace
 
 TEST_CASE("Align differing sizes") {
   StackExposures::ImageAligner aligner;
@@ -38,4 +38,24 @@ TEST_CASE("Align same size") {
   // TODO verify same content.
   auto result2 = aligner.align(img1);
   CHECK(result2 != img1);
+}
+
+TEST_CASE("Unalignable") {
+  // See CMakeLists.txt for TEST_DATA_DIR
+  const std::string data_dir(TEST_DATA_DIR);
+
+  StackExposures::ImageLoader loader;
+
+  auto img1 = loader.load_image(data_dir + "orientation1.jpg");
+  auto img2 = loader.load_image(data_dir + "orientation2.jpg");
+
+  StackExposures::ImageAligner aligner;
+  auto result1 = aligner.align(img1);
+  CHECK(result1 == img1);
+
+  auto result2 = aligner.align(img2);
+  // Alignment should fail, so img2 should be returned unchanged.
+  CHECK(result2 == img2);
+  // TODO capture and verify stderr messages.
+  // https://github.com/catchorg/Catch2/blob/a243cbae52d5599ccce07b36cfe8b2e3308d4108/tests/ExtraTests/X27-CapturedStdoutInTestCaseEvents.cpp
 }
