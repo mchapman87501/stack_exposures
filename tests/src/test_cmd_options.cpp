94cf6a980fde7eeb36fc383750ee6b85b7e7d68b
Mitchell Chapman <mchapman87501@gmail.com>
2022-07-03 11:22:08 -0600

    Capture exit conditions more carefully.  WIP.

--- tests/src/test_cmd_options.cpp
+++ tests/src/test_cmd_options.cpp
@@ -2,13 +2,30 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_exception.hpp>
+
+#include <cstdio>
 #include <iostream>
+#include <sstream>
 
 namespace {
 struct SystemExit : public std::runtime_error {
   const int m_exit_code;
   SystemExit(int exit_code)
       : m_exit_code(exit_code), std::runtime_error(std::to_string(exit_code)) {}
+};
+
+// See https://gist.github.com/AndreLouisCaron/1841061
+class Redirect {
+  std::ostream &m_stream;
+  std::streambuf *const m_buff;
+
+public:
+  Redirect(std::ostream &lhs, std::ostream &rhs = std::cout)
+      : m_stream(rhs), m_buff(m_stream.rdbuf()) {
+    m_stream.rdbuf(lhs.rdbuf());
+  }
+
+  ~Redirect() { m_stream.rdbuf(m_buff); }
 };
 } // namespace
 
@@ -17,47 +34,51 @@ void exit(int status) { throw SystemExit(status); }
 
 namespace {
 
-void no_args() { CmdLine::CmdOptions options({"<test_program>"}); }
+enum class ExitCondition {
+  no_exception,
+  expected_exit_code,
+  unexpected_exit_code
+};
 
-void o_no_value() { CmdLine::CmdOptions opts({"<test_program>", "-o"}); }
-
-void output_no_value() {
-  CmdLine::CmdOptions opts({"<test_program>", "--output"});
+ExitCondition with_options(const CmdLine::ArgVec &args, int expected_code) {
+  try {
+    CmdLine::CmdOptions options(args);
+  } catch (const SystemExit &e) {
+    return (e.m_exit_code == expected_code)
+               ? ExitCondition::expected_exit_code
+               : ExitCondition::unexpected_exit_code;
+  }
+  return ExitCondition::no_exception;
 }
-
-void output_eq_no_value() {
-  CmdLine::CmdOptions opts({"<test_program>", "--output=", "a.jpg", "b.jpg"});
-}
-
 } // namespace
 
 TEST_CASE("empty args") {
-  try {
-    CmdLine::CmdOptions opts({});
-  } catch (const SystemExit &err) {
-    CHECK(err.m_exit_code == 2);
-  }
+  auto result = with_options({}, 2);
+  REQUIRE(result == ExitCondition::expected_exit_code);
 }
+
 TEST_CASE("no args") {
-  REQUIRE_THROWS_MATCHES(no_args(), SystemExit, Catch::Matchers::Message("1"));
+  auto result = with_options({"<test_program>"}, 1);
+  REQUIRE(result == ExitCondition::expected_exit_code);
 }
 
 TEST_CASE("'-o' no value") {
   // See CMakeLists.txt for a feeble attempt to verify expected usage messages.
-  REQUIRE_THROWS_MATCHES(o_no_value(), SystemExit,
-                         Catch::Matchers::Message("1"));
+  auto result = with_options({"<test_program>", "-o"}, 1);
+  REQUIRE(result == ExitCondition::expected_exit_code);
 }
 
 // TEST_CASE names must not start with bare "-" or "--".  If they do, they will
 // annoy/confuse CMake's ctest.
 TEST_CASE("'--output' no value") {
-  REQUIRE_THROWS_MATCHES(output_no_value(), SystemExit,
-                         Catch::Matchers::Message("1"));
+  auto result = with_options({"<test_program>", "--output"}, 1);
+  REQUIRE(result == ExitCondition::expected_exit_code);
 }
 
 TEST_CASE("'--output='<missing_value>") {
-  REQUIRE_THROWS_MATCHES(output_eq_no_value(), SystemExit,
-                         Catch::Matchers::Message("1"));
+  auto result =
+      with_options({"<test_program>", "--output=", "a.jpg", "b.jpg"}, 1);
+  REQUIRE(result == ExitCondition::expected_exit_code);
 }
 
 TEST_CASE("typical usage") {
